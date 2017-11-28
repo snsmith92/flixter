116d0a5210f2a55a8f05b06e2739d7da9323534d
Sherzel Smith <sherzelsmith@gmail.com>
2017-11-28 12:10:41 +0000

    Added validation to instructor course add page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: {minimum: 5, maximum: 70}
+  validates :description, presence: true, length: {minimum: 10}
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end

2dac6ec864fde8ca96c6fd35a4293ec368dce6d4
Sherzel Smith <sherzelsmith@gmail.com>
2017-11-28 17:28:23 +0000

    Added feature to add sections to course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
-
+  has_many :sections
   validates :title, presence: true, length: {minimum: 5, maximum: 70}
   validates :description, presence: true, length: {minimum: 10}
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120145555) do
+ActiveRecord::Schema.define(version: 20171128135144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20171120145555) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

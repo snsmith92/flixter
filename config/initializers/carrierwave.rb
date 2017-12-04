592810bce5b539895e73f6bcedeeaacfe7d69ef5
snsmith92 <sherzelsmith@gmail.com>
2017-12-04 10:52:47 +0100

    Added carrierwave gem and linked to AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:             'AWS',
-    aws_access_key_id:     ENV["AKIAIH65OHC7G5SINLSA"],
-    aws_secret_access_key: ENV["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"],
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
   }
   config.fog_directory = ENV["AWS_BUCKET"]
 end 

b25785a9a59f36c0154fe4855b172978c978af89
snsmith92 <sherzelsmith@gmail.com>
2017-12-05 15:05:09 +0100

    Ensured user authentication for viewing lessons

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,8 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:show]
+
+  
   def new
     @course = Course.new
   end 
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
+
   def show
   end 
 
@@ -8,4 +11,11 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end 
+
+  def require_authorized_for_current_course
+    if current_user.enrolled_in?(current_lesson.section.course) ==false
+      redirect_to course_path(current_lesson.section.course), alert: "You must be enrolled to see lessons."
+    end 
+  end 
+
 end

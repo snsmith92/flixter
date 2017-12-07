5ed308fb7938c332bbad58f5a7b1b196eaed6b5e
snsmith92 <sherzelsmith@gmail.com>
2017-12-07 14:38:29 +0100

    Design changes to instructor course page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -13,7 +13,7 @@ class LessonsController < ApplicationController
   end 
 
   def require_authorized_for_current_course
-    if current_user.enrolled_in?(current_lesson.section.course) ==false
+    if current_lesson.section.course.user != current_user && current_user.enrolled_in?(current_lesson.section.course) ==false
       redirect_to course_path(current_lesson.section.course), alert: "You must be enrolled to see lessons."
     end 
   end 

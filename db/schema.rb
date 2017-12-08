3f471a63c6f529e68203b2c59e1409a53e7e7735
snsmith92 <sherzelsmith@gmail.com>
2017-12-08 10:50:29 +0100

    Added row order to sections within courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207100744) do
+ActiveRecord::Schema.define(version: 20171208093047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20171207100744) do
     t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "row_order"
+    t.index ["row_order"], name: "index_sections_on_row_order", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
